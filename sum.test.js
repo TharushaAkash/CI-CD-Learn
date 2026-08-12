4d4878a67eaeb1a5f997c31ed4e3ed24984d2180
Vithanage T.A <tharushaakasha22@gmail.com>
2026-08-12 11:04:15 +0530

    Add ci pipeline

---
+++ sum.test.js
@@ -0,0 +1,8 @@
+const test = require('node:test');
+const assert = require('node:assert');
+
+const sum = require('./sum');
+
+test('2 + 3 should equal 5', () => {
+    assert.strictEqual(sum(2, 3), 5);
+});
